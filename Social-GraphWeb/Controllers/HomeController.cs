ce8ef789cb7cba85e7a8f2150037f6cb432195ba
Patrick Boudreaux <pboudreaux@daxko.com>
2013-09-18 14:31:39 -0500

    stole some code to use Neo4J projections

--- Social-GraphWeb/Controllers/HomeController.cs
+++ Social-GraphWeb/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Neo4jClient;
 using Neo4jClient.ApiModels.Cypher;
+using Neo4jClient.Cypher;
 using Social_GraphWeb.Graph;
 
 namespace Social_GraphWeb.Controllers
@@ -52,25 +53,93 @@ namespace Social_GraphWeb.Controllers
 			var meReference = new NodeReference<Person>(15, client);
 			var donorReference = new NodeReference<Person>(9, client);
 
-			var query = client.Cypher
-				.Start(new {me=meReference, donor=donorReference})
-			      .Match("p = shortestPath( me-[*..15]->donor )")
-				  .Return<PathsResult>("p");
+
+			// shameless code stealing from http://geekswithblogs.net/cskardon/archive/2013/07/23/neo4jclient-ndash-getting-path-results.aspx
+			ICollection<PathsResult<Person, Knows>> paths = client.PathsBetween<Person, Knows>(meReference, donorReference, 15);
 
 
-			var pathsResult = query.Results.ToList().First();
+			var pathViewModel = new PathViewModel()
+				{
+					MyId = meReference.Id,
+					Path = paths.First()
+				};
+
 			
-			var pathViewModel = new PathViewModel() {RawPath = pathsResult};
-
-			
-
 			return View(pathViewModel);
 		}
     }
 
+	public class PathsResult<TNode, TRelationship> where TRelationship : Relationship, new()
+	{
+		public IEnumerable<Node<TNode>> Nodes { get; set; }
+		public IEnumerable<RelationshipInstance<TRelationship>> Relationships { get; set; }
+	}
+
+	public static class Neo4JClientExtensions
+	{
+		public static ICollection<PathsResult<TNode, TRelationship>> Paths<TNode, TRelationship>(this IGraphClient client, NodeReference<TNode> rootNode, int levels = 1)
+			where TRelationship : Relationship, new()
+		{
+			ICypherFluentQuery<PathsResult<TNode, TRelationship>> pathsQuery = client.Cypher
+				.Start(new { n = rootNode })
+				.Match(string.Format("p=n-[:{0}*1..{1}]->()", new TRelationship().RelationshipTypeKey, levels))
+				.Return(p => new PathsResult<TNode, TRelationship>
+				{
+					Nodes = Return.As<IEnumerable<Node<TNode>>>("nodes(p)"),
+					Relationships = Return.As<IEnumerable<RelationshipInstance<TRelationship>>>("rels(p)")
+				});
+
+			return pathsQuery.Results.ToList();
+		}
+
+		public static ICollection<PathsResult<TNode, TRelationship>> PathsBetween<TNode, TRelationship>(this IGraphClient client, NodeReference<TNode> startNode, NodeReference<TNode> endNode, int levels = 1)
+			where TRelationship : Relationship, new()
+		{
+			ICypherFluentQuery<PathsResult<TNode, TRelationship>> pathsQuery = client.Cypher
+				.Start(new { a = startNode, z = endNode })
+				.Match(string.Format("p=a-[:{0}*1..{1}]->(z)", new TRelationship().RelationshipTypeKey, levels))
+				.Return(p => new PathsResult<TNode, TRelationship>
+				{
+					Nodes = Return.As<IEnumerable<Node<TNode>>>("nodes(p)"),
+					Relationships = Return.As<IEnumerable<RelationshipInstance<TRelationship>>>("rels(p)")
+				});
+
+			return pathsQuery.Results.ToList();
+		}
+	}
+
+	public class Knows : Relationship, IRelationshipAllowingSourceNode<Person>, IRelationshipAllowingTargetNode<Person>
+	{
+		public const string TypeKey = "knows";
+
+		public Knows()
+			: base(-1)
+		{
+		}
+
+		public Knows(NodeReference targetNode)
+			: base(targetNode)
+		{
+		}
+
+		public Knows(NodeReference targetNode, object data)
+			: base(targetNode, data)
+		{
+		}
+
+		public override string RelationshipTypeKey
+		{
+			get { return TypeKey; }
+		}
+	}
+
 	public class PathViewModel
 	{
 		public PathsResult RawPath { get; set; }
+
+		public PathsResult<Person, Knows> Path { get; set; }
+
+		public long MyId { get; set; }
 	}
 
 	public class Person
