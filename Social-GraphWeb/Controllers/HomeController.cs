629ba694cbc9493a1524136ce3da0a734d59fa57
Patrick Boudreaux <pboudreaux@daxko.com>
2013-09-20 15:15:34 -0400

    get alternative routes to our destination

--- Social-GraphWeb/Controllers/HomeController.cs
+++ Social-GraphWeb/Controllers/HomeController.cs
@@ -71,24 +71,17 @@ namespace Social_GraphWeb.Controllers
 
 			var startReference = new NodeReference<Person>(startNodeId, client);
 			var endReference = new NodeReference<Person>(endNodeId, client);
-
-			var pathsQuery = client.Cypher
-			                       .Start(new {a = startReference, z = endReference})
-								   .Match("p=shortestPath(a-[:knows|teaches|takes_class_from|works_with*]->(z))")
-			                       .Return(p => new PathsResult<Person, Knows>
-				                       {
-					                       Nodes = Return.As<IEnumerable<Node<Person>>>("nodes(p)"),
-					                       Relationships = Return.As<IEnumerable<RelationshipInstance<Knows>>>("rels(p)")
-				                       });
-
-			ICollection<PathsResult<Person, Knows>> paths = pathsQuery.Results.ToList();
 			var start = client.Get<Person>(startReference);
 			var end = client.Get<Person>(endReference);
 
+			var shortestPaths = GetShortestPaths(client, startReference, endReference);
+
+			var allPaths = GetAllPaths(client, startReference, endReference);
+
 			int countOfHops = 0;
-			var path = paths.FirstOrDefault();
-			if (path != null && path.Nodes.Any())
-				countOfHops = path.Nodes.Count() - 1;
+			var shortestPath = shortestPaths.FirstOrDefault();
+			if (shortestPath != null && shortestPath.Nodes.Any())
+				countOfHops = shortestPath.Nodes.Count() - 1;
 
 			var pathViewModel = new PathViewModel()
 				{
@@ -97,10 +90,43 @@ namespace Social_GraphWeb.Controllers
 					Target = end,
 					TargetId = endReference.Id,
 					CountOfHops = countOfHops,
-					ShortestPath = path
+					ShortestPath = shortestPath,
+
+					AllPaths = allPaths
 				};
 
 			return pathViewModel;
+		}
+
+	    private static IEnumerable<PathsResult<Person, Knows>> GetShortestPaths(GraphClient client, NodeReference<Person> startReference, NodeReference<Person> endReference)
+	    {
+		    var shortestPathsQuery = client.Cypher
+		                                   .Start(new {a = startReference, z = endReference})
+		                                   .Match("p=shortestPath(a-[:knows|teaches|takes_class_from|works_with*]->(z))")
+		                                   .Return(p => new PathsResult<Person, Knows>
+			                                   {
+				                                   Nodes = Return.As<IEnumerable<Node<Person>>>("nodes(p)"),
+				                                   Relationships = Return.As<IEnumerable<RelationshipInstance<Knows>>>("rels(p)")
+			                                   });
+
+		    ICollection<PathsResult<Person, Knows>> shortestPaths = shortestPathsQuery.Results.ToList();
+		    return shortestPaths;
+	    }
+
+		private static IEnumerable<PathsResult<Person, Knows>> GetAllPaths(GraphClient client, NodeReference<Person> startReference, NodeReference<Person> endReference)
+		{
+			var allPathsQuery = client.Cypher
+										   .Start(new { a = startReference, z = endReference })
+										   .Match("p=a-[:knows|teaches|takes_class_from|works_with*1..7]->(z)")
+										   .Where("ALL (x IN nodes(p) WHERE SINGLE (x2 IN nodes(p) WHERE x=x2))")
+										   .Return(p => new PathsResult<Person, Knows>
+										   {
+											   Nodes = Return.As<IEnumerable<Node<Person>>>("nodes(p)"),
+											   Relationships = Return.As<IEnumerable<RelationshipInstance<Knows>>>("rels(p)")
+										   });
+
+			ICollection<PathsResult<Person, Knows>> shortestPaths = allPathsQuery.Results.ToList();
+			return shortestPaths;
 		}
     }
 	
@@ -125,6 +151,8 @@ namespace Social_GraphWeb.Controllers
 		public Node<Person> Target { get; set; }
 
 		public int CountOfHops { get; set; }
+
+		public IEnumerable<PathsResult<Person, Knows>> AllPaths { get; set; }
 
 		public string NodeName(Node<Person> node)
 		{
